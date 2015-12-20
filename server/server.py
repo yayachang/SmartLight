a1d3d3fdc524c29b4fd3fe39dc4ce33fddbedc6f
YungHsin <yhchang0921@gmail.com>
2015-12-20 21:25:25 +0800

    Client, Server ,ESP8266 connect together

--- server/server.py
+++ server/server.py
@@ -1,54 +1,64 @@
 #!/usr/bin/python
 import socket
 import commands
+import datetime
+
+TAG='[ETB]'
 HOST='0.0.0.0'
 PORT=50007
 
-ESP_HOST = '192.168.1.21'
+ESP_HOST = '192.168.1.5'
 ESP_PORT = 1234
 
 def runLightServer():
-	try:
-		global irc
-		irc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		irc.bind((HOST,PORT))
-		irc.listen(1)
-	except socket.error:
-		print ('server socket.error, reconnect...')
-		runLightServer()
-
+	global serverSocket
+	serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	serverSocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) 
+	serverSocket.bind((HOST,PORT))
+	serverSocket.listen(1)
+	
 def connectESP8266():
 	try:
-		global irc8266
-		irc8266=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		irc8266.connect((ESP_HOST,ESP_PORT))
-		irc8266.settimeout(5)
+		global espSocket
+		espSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		espSocket.connect((ESP_HOST,ESP_PORT))
+		espSocket.settimeout(5)
 	except socket.error:
-		print('ESP8266 socket.error, recconect...')
+		print(str(datetime.datetime.now())+':'+TAG+'ESP8266 socket.error, recconect...')
 		connectESP8266()
 
 runLightServer()
 connectESP8266()
 
+
 while 1:
-	print '------waiting------'
+	#wait App send command
+	print str(datetime.datetime.now())+':'+'------waiting------\n'
+	connection,address=serverSocket.accept()
+	sendData=connection.recv(3)
+	print str(datetime.datetime.now())+':'+TAG+'Connected '+address[0] + ':' + str(address[1])+' -- send data:'+sendData
+	
 	try:
-		conn,addr=irc.accept()
-		data=conn.recv(1024)
-		print 'Connected '+addr[0] + ':' + str(addr[1])+',receive:'+data
-	except:
-		print('Light server error')
-		print('restart light server')
-		runLightServer()
-	try:
-		if data:
-			irc8266.sendall('1')
+		#send command to esp8266
+		if sendData == '0':
+			espSocket.sendall('0')
+		elif sendData == '1':
+			espSocket.sendall('1')
+		elif sendData == '2':
+			espSocket.sendall('2')
 		else:
-			irc8266.sendall('0')
-		data2 = irc8266.recv(1024)
-		print('ESP8266 response:{}'.format(data2)+'\n')
+			espSocket.sendall('0')
+		print str(datetime.datetime.now())+':'+TAG+'Send data to ESP8266:'+sendData
+		
+		#receive response from esp8266
+		esp8266Response = espSocket.recv(1024)
+		print(str(datetime.datetime.now())+':'+TAG+'ESP8266 response:{}'.format(esp8266Response)+'\n')
+		#send response to App
+		connection.sendall(esp8266Response)
+
+		connection.close()
 	except:
-		print('ESP8266 error')
-		print('reconnect ESP8266')
+		print(str(datetime.datetime.now())+':'+TAG+'ESP8266 error')
+		print(str(datetime.datetime.now())+':'+TAG+'reconnect ESP8266')
 		connectESP8266()
-conn.close()
+connection.close()
